a495f3637b741b622d3ea5461e6239c5f32cac93
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-04-17 06:13:34 +0300

    Update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,4 +6,5 @@ from gendiff.formats.format import choice_format
 def generate_diff(first_path, second_path, format):
     first_file = load_file_from_path(first_path)
     second_file = load_file_from_path(second_path)
-    return choice_format(generator(first_file, second_file), format)
+    diff = generator(first_file, second_file)
+    return choice_format(diff, format)
