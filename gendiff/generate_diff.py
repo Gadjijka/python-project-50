a5ba927611d75ce7b76eee8f0e86e4152c54f7c7
Gajijka <galimovgadjimurad2020@gmail.com>
2024-04-17 05:02:19 +0300

    Changing gendiff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,8 +1,9 @@
 from gendiff.base_logic import base_logic
 from gendiff.parser import load_file_from_path
+from gendiff.formats.format import choice_format
 
 
-def generate_diff(first_path, second_path):
+def generate_diff(first_path, second_path, format):
     first_file = load_file_from_path(first_path)
     second_file = load_file_from_path(second_path)
-    return base_logic(first_file, second_file)
+    return choice_format(base_logic(first_file, second_file), format)
