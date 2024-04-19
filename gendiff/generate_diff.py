959cc90c2e41afb0c50b4f5b9fb59729a3a177f3
Gajijka <galimovgadjimurad2020@gmail.com>
2024-04-19 16:59:55 +0300

    Have changed project

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,6 @@
 from gendiff.generator import generator
 from gendiff.parser import load_file_from_path
-from gendiff.formats.format import choice_format
+from gendiff.formatters.format import choice_format
 
 
 def generate_diff(first_path, second_path, format='stylish'):
