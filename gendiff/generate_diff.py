c837d8b8c51425688d0f609cb5e20e9811a45aa4
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-04-20 23:11:58 +0300

    Update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,10 @@
 from gendiff.generator import generator
 from gendiff.parser import load_file_from_path
-from gendiff.formatters import choice_format
+from gendiff.formatters import *
 
 
 def generate_diff(first_path, second_path, format='stylish'):
     first_file = load_file_from_path(first_path)
     second_file = load_file_from_path(second_path)
     diff = generator(first_file, second_file)
-    return choice_format(diff, format)
+    return gendiff.formatters(diff, format)
